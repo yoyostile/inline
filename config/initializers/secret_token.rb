3477b97f8412a0ed01f8d10ac7d8df72fdd55c95
Johannes Heck <yoyostile@gmail.com>
2013-04-10 20:07:47 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inline::Application.config.secret_token = 'e246184dbab8d3d0e884b7d3db9699da503b72de64c84f3deba9a885fb226bf179ce0922dea53d4291ab4463fbd0a429f069fdf01de1e62e8b22e321086da75f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Inline::Application.config.session_store :cookie_store, key: '_inline_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Inline::Application.config.session_store :active_record_store
